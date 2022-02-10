dd55a92c8e80f8e1567fa13e234af17b2881c72e
LuSUUUYuuu <19906127599@163.com>
2022-02-10 23:31:57 +0800

    两数之和

--- leetCodeDay1/src/TwoSums.java
+++ leetCodeDay1/src/TwoSums.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,10 +7,26 @@ public class TwoSums {
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; ++i) {
+            System.out.println("i----->" + i);
+            System.out.println("nums[" + i + "]----->" + nums[i]);
+            System.out.println("target - nums[i]---->" + (target - nums[i]));
             if (hashtable.containsKey(target - nums[i])) {
                 return new int[]{hashtable.get(target - nums[i]), i};
             }
             hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static int[] twoSum0(int[] nums, int target){
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (hashtable.containsKey(target - num)){
+                return new int[]{hashtable.get(target - num), i};
+            }
+            hashtable.put(num, i);
+
         }
         return new int[0];
     }
